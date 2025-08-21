dd40f7c5d3a79b902d413905b5aef961ea50775f
Sahil Ijaz <hssahil2913@gmail.com>
2025-08-21 18:16:00 +0500

    ok

--- components/buttonComponent.tsx
+++ components/buttonComponent.tsx
@@ -47,8 +47,8 @@ const ButtonComponent: React.FC<buttonProps> = ({
         type={type}
         className={clsx(
           actionButton &&
-            "text-white hover:bg-blue-800 focus:ring-4 font-medium text-sm px-5 py-2 me-2 mb-2 focus:outline-none",
-          `${bgColor} hover:${bgColor} font-medium text-sm px-5 py-2 me-2 mb-2 focus:outline-none`
+            "text-white hover:bg-blue-800 focus:ring-4 rounded-2xl font-medium text-sm px-5 py-2 me-2 mb-2 focus:outline-none",
+          `${bgColor} hover:${bgColor} font-medium text-sm rounded-2xl px-5 py-2 me-2 mb-2 focus:outline-none`
         )}
         {...props}
       >
