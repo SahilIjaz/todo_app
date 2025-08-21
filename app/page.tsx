6bef95702e30b3e8e70ecb60de3b93ae94db6eee
Sahil Ijaz <hssahil2913@gmail.com>
2025-08-22 02:44:37 +0500

    project completed

--- app/page.tsx
+++ app/page.tsx
@@ -25,11 +25,12 @@ export default async function Home() {
     <div className="w-screen py-20 flex justify-center flex-col items-center">
       <span className="text-4xl font-extrabold uppercase">Todo App</span>
       <h1 className="text-5xl font-extrabold uppercase mb-5 text-center">
-        <span className="lowercase">w/</span>Server Actions
+        <span className="lowercase text-sm">full stack by using next.js</span>
       </h1>
       <div className="flex justify-center flex-col items-center">
         {}
         <AddTodo />
+        
         <div className="flex flex-col gap-5 items-center justify-center mt-10 w-screen"></div>
         {data.map((todo, id) => (
           <div key={id}>
