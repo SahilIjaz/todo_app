3f3a257d851fe2e09660e7128d452459311f2451
Sahil Ijaz <hssahil2913@gmail.com>
2025-08-23 22:37:33 +0500

    2

--- components/changeTodo.tsx
+++ components/changeTodo.tsx
@@ -3,6 +3,7 @@ import * as actions from "@/actions";
 import { todoProps } from "@/types";
 import { FaCheck } from "react-icons/fa6";
 
+
 import Button from "@/components/buttonComponent"; // make sure correct path
 
 function ChangeTodo({ todo }: { todo: todoProps }) {
