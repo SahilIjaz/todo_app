0a5da31953fc8d7d0131b2cb2dce184f80deaff7
Sahil Ijaz <hssahil2913@gmail.com>
2025-08-25 01:04:46 +0500

    13

--- components/changeTodo.tsx
+++ components/changeTodo.tsx
@@ -14,8 +14,7 @@ function ChangeTodo({ todo }: { todo: todoProps }) {
        
         
         
-
-        
+       
         actionButton
         bgColor={todo.isCompleted ? "bg-green-400" : "bg-blue-500"}
       />
